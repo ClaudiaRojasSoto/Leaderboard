9899a3bb34c50ca9bbd352fb2b45cc81588d70a1
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-06-21 13:51:16 -0400

    Fix linter errors about console use importin the function handleFetchError on errorHandle.js file on modules folder

--- src/index.js
+++ src/index.js
@@ -1,41 +1,36 @@
-import './style.css'
-import { createNewGame } from './modules/api';
-import { refreshScores, submitScore } from './modules/ui';
+import './style.css';
+import { createNewGame } from './modules/api.js';
+import { refreshScores, submitScore } from './modules/ui.js';
+import handleFetchError from './modules/errorHandler.js';
 
-// Async function to start the game
 async function initGame() {
-  // Get the game id from local storage if it exists, otherwise create a new game
   let gameId = localStorage.getItem('gameId');
   if (!gameId) {
     gameId = await createNewGame('House to House');
     if (!gameId) {
-      console.error('Failed to create a new game. Check your internet connection and try again.');
+      handleFetchError(new Error('Failed to create a new game. Check your internet connection and try again.'));
     }
   }
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
   const refreshButton = document.querySelector('.refresh-button');
   const submitButton = document.querySelector('.submit-button');
   const nameInput = document.querySelector('.name-input');
   const scoreInput = document.querySelector('.score-input');
   const scoresList = document.querySelector('.scores-list');
 
-  // Event to "Refresh" button
   refreshButton.addEventListener('click', () => {
     refreshScores(scoresList);
   });
 
-  // Event to "Submit" button
   submitButton.addEventListener('click', () => {
     const userName = nameInput.value;
     const score = scoreInput.value;
     submitScore(userName, score);
-    // Clean the inputs after submit
     nameInput.value = '';
     scoreInput.value = '';
   });
 
-  // Initialize the game
   initGame();
 });
