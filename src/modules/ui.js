4f183d9b424509bdac85b77436730c52e77d88db
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-06-21 13:53:35 -0400

    Add the js extension on the folder imported on line '1'

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,24 +1,23 @@
-import { getGameScores, postGameScore } from './api';
+import { getGameScores, postGameScore } from './api.js';
 
 export const refreshScores = async (scoresList) => {
-    const scores = await getGameScores();
-  
-    // Clear the scores list
-    while (scoresList.firstChild) {
-      scoresList.firstChild.remove();
-    }
-  
-    // Sort scores from highest to lowest
-    scores.sort((a, b) => b.score - a.score);
-  
-    // Add new scores to the list
-    scores.forEach((score) => {
-      const li = document.createElement('li');
-      li.textContent = `${score.user}: ${score.score}`;
-      scoresList.appendChild(li);
-    });
-  };
-  
+  const scores = await getGameScores();
+
+  // Clear the scores list
+  while (scoresList.firstChild) {
+    scoresList.firstChild.remove();
+  }
+
+  // Sort scores from highest to lowest
+  scores.sort((a, b) => b.score - a.score);
+
+  // Add new scores to the list
+  scores.forEach((score) => {
+    const li = document.createElement('li');
+    li.textContent = `${score.user}: ${score.score}`;
+    scoresList.appendChild(li);
+  });
+};
 
 export const submitScore = async (userName, score) => {
   await postGameScore(userName, score);
