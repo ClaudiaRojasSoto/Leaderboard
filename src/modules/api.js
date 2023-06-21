3c65cdb482ba2504d0d52bf323f454d092b56ee0
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-06-21 12:39:00 -0400

    Add manipulations for basic errors in the app on api.js file on src folder

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,41 +1,74 @@
 const API_BASE_URL =
   'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
 
-let gameId; // To storage the ID of the game
-
 export const createNewGame = async (gameName) => {
-  const response = await fetch(`${API_BASE_URL}games/`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      name: gameName,
-    }),
-  })
+  try {
+    const response = await fetch(`${API_BASE_URL}games/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: gameName,
+      }),
+    });
 
-  const data = await response.json();
-  gameId = data.result.split(': ')[1]; // Storage the ID of the game
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const gameId = data.result.split(': ')[1]; // Storage the ID of the game
+
+    // Store the game id in local storage
+    localStorage.setItem('gameId', gameId);
+
+    return gameId;
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
 };
 
 export const getGameScores = async () => {
-  const response = await fetch(`${API_BASE_URL}games/${gameId}/scores`);
-  const data = await response.json();
-  return data.result;
+  try {
+    // Get the game id from local storage
+    const gameId = localStorage.getItem('gameId');
+
+    const response = await fetch(`${API_BASE_URL}games/${gameId}/scores`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.result;
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
 };
 
 export const postGameScore = async (userName, score) => {
-  const response = await fetch(`${API_BASE_URL}games/${gameId}/scores`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      user: userName,
-      score: score,
-    }),
-  });
+  try {
+    // Get the game id from local storage
+    const gameId = localStorage.getItem('gameId');
 
-  const data = await response.json();
-  return data.result;
+    const response = await fetch(`${API_BASE_URL}games/${gameId}/scores`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user: userName,
+        score: score,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.result;
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
 };
