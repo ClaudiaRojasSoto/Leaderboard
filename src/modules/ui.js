194e756ac7eb417825dc281153e449a6d3c3cf90
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-06-21 12:36:40 -0400

    Change the functionality of the refreshScores function to clear the scores at first, then call the API and orderer the scores on ui.js file on src folder

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,20 +1,25 @@
 import { getGameScores, postGameScore } from './api';
 
 export const refreshScores = async (scoresList) => {
-  const scores = await getGameScores();
-  // Clean the score list
-  scoresList.innerHTML = '';
-  // Add new scores to the list
-  scores.forEach((score) => {
-    const li = document.createElement('li');
-    li.textContent = `${score.user}: ${score.score}`;
-    scoresList.appendChild(li);
-  });
-};
+    const scores = await getGameScores();
+  
+    // Clear the scores list
+    while (scoresList.firstChild) {
+      scoresList.firstChild.remove();
+    }
+  
+    // Sort scores from highest to lowest
+    scores.sort((a, b) => b.score - a.score);
+  
+    // Add new scores to the list
+    scores.forEach((score) => {
+      const li = document.createElement('li');
+      li.textContent = `${score.user}: ${score.score}`;
+      scoresList.appendChild(li);
+    });
+  };
+  
 
 export const submitScore = async (userName, score) => {
   await postGameScore(userName, score);
-  // Clean the inputs after submit
-  userName.value = '';
-  score.value = '';
 };
