f6013bac9f7a195e54b2ca4071d42c76663b289d
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-06-23 16:19:07 -0400

    Revert "Change the rules for the image in modules of the webpack"
    This reverts commit 35e695bd105157b23581752d8bde503f8b117784.

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|jpg|gif|svg)$/i,
+        test: /\.(png|jpe?g|gif|svg)$/i,
         type: 'asset/resource',
         generator: {
           filename: 'images/[name][ext]',
